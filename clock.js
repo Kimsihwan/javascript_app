4a47f77bb1c4bbb338211b1ae7f4302f87b57062
Kimsihwan <namid882@gmail.com>
2018-11-24 22:34:28 +0900

    00표시

--- clock.js
+++ clock.js
@@ -6,11 +6,14 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`; // 삼항 연산자 세컨즈가 10보다 작으면 `0${seconds}` 실행 그렇지 않으면 seconds 실행
 }
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
